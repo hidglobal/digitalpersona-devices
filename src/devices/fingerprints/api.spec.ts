2f12f4d7c983b82e1560dc00cd7cd792f72a953a
Andrey Bronnikov <andreyb@digitalpersona.com>
2019-03-28 02:25:08 -0700

    + unit tests for fingerprints api

---
+++ src/devices/fingerprints/api.spec.ts
@@ -0,0 +1,25 @@
+import { FingerprintsApi } from './api';
+
+describe("FingerprintsApi: ", () => {
+
+    let api: FingerprintsApi;
+    beforeEach(()=>{
+        api = new FingerprintsApi();
+    })
+
+    it("must enumerate devices", async ()=>{
+        const devices = await api.enumerateDevices();
+        expect(devices.length).toBeGreaterThanOrEqual(1);
+    })
+
+    it("must obtain Device info", async()=> {
+        const devices = await api.enumerateDevices();
+        const info = await api.getDeviceInfo(devices[0]);
+        expect(info).toBeDefined();
+        expect(info!.DeviceID).toBeDefined();
+        expect(info!.eDeviceModality).toBeDefined();
+        expect(info!.eDeviceTech).toBeDefined();
+        expect(info!.eUidType).toBeDefined();
+    })
+
+})
