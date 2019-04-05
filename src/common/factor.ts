34eea23d70d9d43679265157e19cec1e87d7326e
Andrey Bronnikov <andreyb@digitalpersona.com>
2019-04-05 02:17:16 -0700

    + BioSample structures and enums

---
+++ src/common/factor.ts
@@ -0,0 +1,85 @@
+import { Base64UrlString, Base64Url } from "@digitalpersona/access-management";
+
+export enum BioFactor
+{
+    Multiple            = 0x0001,
+    FacialFeatures      = 0x0002,
+    Voice               = 0x0004,
+    Fingerprint         = 0x0008,
+    Iris                = 0x0010,
+    Retina              = 0x0020,
+    HandGeometry        = 0x0040,
+    SignatureDynamics   = 0x0080,
+    KEYSTROKE_DYNAMICS  = 0x0100,
+    LipMovement         = 0x0200,
+    ThermalFaceImage    = 0x0400,
+    ThermalHandImage    = 0x0800,
+    Gait                = 0x1000,
+}
+
+// Biometric owner ID registered with IBIA (http://www.ibia.org/base/cbeff/_biometric_org.phpx).
+export enum BioSampleFormatOwner
+{
+    None                = 0,
+    Neurotechnologija   = 49,   // fingerprints
+    DigitalPersona      = 51,   // fingerprints
+    Cognitec            = 99,   // face
+    Innovatrics         = 53,   // face
+}
+
+export class BioSampleFormat
+{
+    constructor(
+        public readonly FormatOwner: BioSampleFormatOwner,
+        public readonly FormatID: number,                        // Vendor specific format ID
+    ){}
+}
+
+export enum BioSampleType
+{
+    Raw                 = 0x01,   // Fingerprint image
+    Intermadiate        = 0x02,   // Fingerprint feature set
+    Processed           = 0x04,   // Fingerprint template
+    RawWSQCompressed    = 0x08,   // WSQ compressed image
+    Encrypted           = 0x10,
+    Signed              = 0x20,
+}
+
+export enum BioSamplePurpose
+{
+    Any                         = 0,
+    Verify                      = 1,
+    Identify                    = 2,
+    Enroll                      = 3,
+    EnrollForVerificationOnly   = 4,
+    EnrollForIdentificationOnly = 5,
+    Audit                       = 6
+}
+
+export enum BioSampleEncryption
+{
+    Nnone   = 0,    // Data is not encrypted
+    XTEA    = 1,    // XTEA encryption with well known key
+}
+
+export class BioSampleHeader
+{
+    constructor(
+        public Factor: BioFactor,               // Biometric factor. Must be set to 8 for fingerprint
+        public Format: BioSampleFormat,         // Format owner (vendor) information
+        public Type: BioSampleType,             // type of biometric sample
+        public Purpose: BioSamplePurpose,       // Purpose of biometric sample
+        public Quality: number,                 // Quality of biometric sample. If we don't support quality it should be set to -1.
+        public Encryption: BioSampleEncryption, // Encryption of biometric sample.
+    ){}
+}
+
+export class BioSample
+{
+    public readonly Version = 1;
+
+    constructor(
+        public readonly Header: BioSampleHeader,    // Biometric sample header
+        public readonly Data: Base64UrlString,      // Base64url encoded biometric sample data
+    ){}
+}
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,2 +1,3 @@
+export * from './factor';
 export * from './eventSource';
 export * from './events';
