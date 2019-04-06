3392f0eb39796a31f0400a8d453a8b19b17a6eae
Andrey Bronnikov <andreyb@digitalpersona.com>
2019-04-06 01:35:53 -0700

    + password token

---
+++ src/tokens/password/api.ts
@@ -0,0 +1,43 @@
+import { User, Ticket, IAuthService, IEnrollService, JSONWebToken } from '@digitalpersona/access-management';
+import { CustomAction } from './actions';
+import { Password } from './credential';
+
+export class PasswordApi
+{
+    constructor(
+        private readonly authService: IAuthService,
+        private readonly enrollService: IEnrollService,
+        private readonly securityOfficer: JSONWebToken,
+    ){}
+
+    authenticate(user: User, password: string): Promise<JSONWebToken> {
+        return this.authService
+            .AuthenticateUser(user, new Password(password))
+            .then(ticket => ticket.jwt);
+    }
+
+    enroll(user: JSONWebToken, password: string, oldPassword: string|null = null, securityOfficer?: JSONWebToken): Promise<void> {
+        if (!this.enrollService)
+            return Promise.reject(new Error("enrollService"));
+        return this.enrollService
+            .EnrollUserCredentials(
+                new Ticket(securityOfficer || this.securityOfficer || user),
+                new Ticket(user),
+                new Password(password));
+    }
+
+    randomize(user: User, securityOfficer?: JSONWebToken): Promise<string> {
+        return this.authService.CustomAction(
+            CustomAction.PasswordRandomization,
+            new Ticket(securityOfficer || this.securityOfficer),
+            user);
+    }
+
+    reset(user: User, newPassword: string, securityOfficer?: JSONWebToken): Promise<string> {
+        return this.authService.CustomAction(
+            CustomAction.PasswordReset,
+            new Ticket(securityOfficer || this.securityOfficer),
+            user,
+            new Password(newPassword));
+    }
+}
---
+++ src/tokens/password/credential.ts
@@ -0,0 +1,12 @@
+import { Credential, Base64Url } from "@digitalpersona/access-management";
+
+export class Password extends Credential
+{
+    constructor(password: string, oldPassword?: string) {
+        super(Credential.Password, Base64Url.fromUtf16(JSON.stringify(
+            typeof oldPassword !== 'undefined'
+                ? { oldPassword, newPassword: password }    // password change/reset
+                : password                                  // password authentication
+        )));
+    }
+}
